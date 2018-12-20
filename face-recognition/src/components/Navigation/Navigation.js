dd8d97e1348c109bbc1b87156ca64942e7cf96b6
lajawanti <myemail>
2018-12-20 17:23:01 -0500

    added navigation component

---
+++ face-recognition/src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <nav style = {{display : 'flex', justifyContent : 'flex-end'}}>
+                <p className = 'f4 link dim underline pa4 pointer'>Sign Out</p>
+            </nav>        
+        </div>
+    )
+}
+
+export default Navigation;
